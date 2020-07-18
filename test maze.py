cb6997943f2da59b98161d32cf2a28f1f95d83cd
code-cultivater <352377811@qq.com>
2020-07-18 15:06:49 +0800

    first runable version

--- test maze.py
+++ test maze.py
@@ -7,8 +7,6 @@ for j in range(2):
     env.reset()
 
     env.render()
-    [4,2,4,2]
-    [3,1,3,1]
     actions=[[4,3],[2,1],[4,3],[2,1]]
     for i in range(len(actions)):
         ne_obs, re, done, info = env.step(actions[i])
